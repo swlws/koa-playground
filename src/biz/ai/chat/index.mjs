121ec2fa15771cf240017ac6f9537116a81c102d
swlws <979623074@qq.com>
2025-12-16 16:08:05 +0800

    doc

--- src/biz/ai/chat/index.mjs
+++ src/biz/ai/chat/index.mjs
@@ -1,3 +1,6 @@
+// API 文档
+// https://help.aliyun.com/zh/model-studio/qwen-api-reference?spm=a2c4g.11186623.help-menu-2400256.d_2_1_0.336f51573msTqf&scm=20140722.H_2712576._.OR_help-T_cn~zh-V_1#4ec3e641c294d
+
 import OpenAI from 'openai';
 import { aiConfig, rolePrompts } from './config.mjs';
 
